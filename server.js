c2b230ef7616e5e7e33575e3a906cf5c1b3cf444
Faisal Hussain <faisal@base29.com>
2021-04-16 17:26:19 +0500

    added socket.io package and initialzed a listner

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 // Set Static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', (socket) => {
+    console.log('New Connection');
+});
 
 const PORT = 3000 || process.env.PORT;
 
